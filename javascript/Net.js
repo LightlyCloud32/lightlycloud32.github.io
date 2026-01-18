961760c733c5ccdb976ff7546cb9a1cc1e193962
LightlyCloud32 <LeafDuvetPrivate@outlook.jp>
2026-01-19 01:52:11 +0900

    Update

---
+++ javascript/Net.js
@@ -0,0 +1,16 @@
+class Net {
+  /**Webサーバーのコンテンツを取得する @param {string} url 取得先のURL*/
+  async fetch(url) {
+    return (await (await fetch(url)).text()).toString();
+  }
+  /**ファイルを取得する @param {string} path ディレクトリ @param {string | null} param パラメーター*/
+  async fetchFile(path, param) {
+    const url = `${location.origin}${location.pathname}${path}/${param}`;
+    var f = await this.fetch(url);
+    //エラー処理(意味的に当てはまるエラーメッセージ)
+    if(!param) f = '<h1 id="Error">400 Bad Request</h1>';
+    if(f.includes('404')) f = '<h1 id="Error">404 Not Found</h1>';
+    return f;
+  }
+}
+export default new Net();
--- javascript/ViewerUtils.js
+++ javascript/ViewerUtils.js
@@ -1,27 +1,15 @@
 import ParamsManager from './ParamsManager.js';
+import Net from './Net.js';
 /**サイトのユーティリティー*/
 class ViewerUtils {
   constructor() {
     this.domParser = new DOMParser();
     /**URLパラメーター管理*/
     this.paramsManager = ParamsManager;
+    this.net = Net;
   }
 
   //#region 取得系のユーティリティー
-  /**Webサーバーのコンテンツを取得する @param {string} url 取得先のURL*/
-  async fetch(url) {
-    return (await (await fetch(url)).text()).toString();
-  }
-  /**ファイルを取得する @param {string} path ディレクトリ*/
-  async fetchFile(path) {
-    const param = this.paramsManager.getParam('f'); //ファイルパラメーターの取得
-    const url = `${location.origin}${location.pathname}${path}/${param}`;
-    var f = await this.fetch(url);
-    //エラー処理(意味的に当てはまるエラーメッセージ)
-    if(!param) f = '<h1 id="Error">400 Bad Request</h1>';
-    if(f.includes('404')) f = '<h1 id="Error">404 Not Found</h1>';
-    return f;
-  }
   /**DOMをHTMLElementに変換 @param {string} dom DOM*/
   domParse(dom) {
     return this.domParser.parseFromString(dom, 'text/html');
