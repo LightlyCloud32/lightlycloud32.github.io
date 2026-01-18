d42e898d2a7856459c55c0bbeb6773f6f7d01b8f
LightlyCloud32 <LeafDuvetPrivate@outlook.jp>
2026-01-18 23:22:56 +0900

    Update
    Null

--- javascript/ViewerUtils.js
+++ javascript/ViewerUtils.js
@@ -12,6 +12,16 @@ class ViewerUtils {
   async fetch(url) {
     return (await (await fetch(url)).text()).toString();
   }
+  /**ファイルを取得する @param {string} path ディレクトリ*/
+  async fetchFile(path) {
+    const param = this.paramsManager.getParam('f'); //ファイルパラメーターの取得
+    const url = `${location.origin}${location.pathname}${path}/${param}`;
+    var f = await this.fetch(url);
+    //エラー処理
+    if(!param) f = '<h1>Null</h1>';
+    if(f.includes('404')) f = '<h1>404 Not Found</h1>';
+    return f;
+  }
   /**DOMをHTMLElementに変換 @param {string} dom DOM*/
   domParse(dom) {
     return this.domParser.parseFromString(dom, 'text/html');
